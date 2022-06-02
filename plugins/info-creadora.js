0a8c66a96d7492b92c2a79f9e271bfe9a8aeec46
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-01 19:28:30 -0500

    Add files via upload

---
+++ plugins/info-creadora.js
@@ -0,0 +1,29 @@
+import fs from 'fs'
+function handler(m, { conn }) {
+let text = `
+*𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤 | 𝘾𝙤𝙣𝙩𝙖𝙘𝙩* 
+*Wa.me/593993684821 (BOT)*
+*https://www.instagram.com/gata_dios*
+`.trim()   
+conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `https://wa.me/593993684821`}}})
+  
+//const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+//this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+  
+let pp = './media/menus/Menu2.jpg'
+let str = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`
+
+conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/gata_dios', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+['𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
+['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu']
+], m,)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+handler.command = /^(contacto|owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
+export default handler
