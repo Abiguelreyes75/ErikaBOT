89c74fa4984de293a97ae5f7ad6345425f3cb8a8
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-02 23:38:35 -0500

    Update vaginas.js

--- plugins/vaginas.js
+++ plugins/vaginas.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw'
-let url = pack[Math.floor(Math.random() * pack.length)]
+let url = vagina[Math.floor(Math.random() * pack.length)]
 conn.sendButton(m.chat, `🥵 VAGINA 🥵`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 }
 handler.help = ['vagina']
@@ -9,7 +9,7 @@ handler.tags = ['internet']
 handler.command = /^(vagina)$/i
 export default handler
 
-global.pack = [
+global.vagina = [
   "https://i.pinimg.com/736x/12/87/1b/12871b6e8385373148359ec16391f60f.jpg",
   "https://data.pixiz.com/output/user/frame/preview/400x400/9/0/4/3/3513409_00fe2.jpg",
   "https://i.pinimg.com/736x/47/cd/a8/47cda8eca5f5f013d14ce7dd6b408bfd.jpg",
