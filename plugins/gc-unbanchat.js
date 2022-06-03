85f742eddb518e0e60d28b220075d24ab3ea0f75
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-03 00:01:41 -0500

    Update gc-unbanchat.js

--- plugins/gc-unbanchat.js
+++ plugins/gc-unbanchat.js
@@ -1,5 +1,5 @@
 let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = true
+global.db.data.chats[m.chat].isBanned = false
 m.reply('[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾')
 }
 handler.help = ['unbanchat']
