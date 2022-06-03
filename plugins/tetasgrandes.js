bfdc7bfd2cee1f262a997433fbc97ff06d623115
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-02 23:17:22 -0500

    Update tetasgrandes.js

--- plugins/tetasgrandes.js
+++ plugins/tetasgrandes.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
-if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw'
 let url = pack[Math.floor(Math.random() * pack.length)]
-conn.sendButton(m.chat, `_🥵 tetasgrandes 🥵_`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+conn.sendButton(m.chat, `🥵 Tetas Grandes 🥵`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 }
 handler.help = ['tetasgrandes']
 handler.tags = ['internet']
@@ -10,6 +10,7 @@ handler.command = /^(tetasgrandes)$/i
 export default handler
 
 global.pack = [
-  "https://thumb-v2.xhcdn.com/a/vamJ40FMwvU0FE4UfctZAw/018/883/602/320x240.10.jpg",
-  "https://cdn77-pic.xnxx-cdn.com/videos/thumbslll/b6/03/2c/b6032c8938086eb4b43a1aaa7f7b4f0f/b6032c8938086eb4b43a1aaa7f7b4f0f.3.jpg",
+  "https://www.copasmenstruales.com/blog/wp-content/uploads/Mal-olor-vaginal-768x512-1.jpg",
+  "https://image.shutterstock.com/image-vector/daisy-flower-cute-bee-cartoon-260nw-1891391209.jpg",
+  "https://i.pinimg.com/236x/2c/04/98/2c0498de02a52723c392ea08a149791d.jpg",
 ]
