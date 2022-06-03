010564e2c676cf7c18c6c74e82887f053c9a823e
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-02 23:47:11 -0500

    Update tetasgrandes.js

--- plugins/tetasgrandes.js
+++ plugins/tetasgrandes.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw'
-let url = pack[Math.floor(Math.random() * pack.length)]
+let url = tetasgrandes[Math.floor(Math.random() * tetasgrandes.length)]
 conn.sendButton(m.chat, `🥵 Tetas Grandes 🥵`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 }
 handler.help = ['tetasgrandes']
@@ -9,7 +9,7 @@ handler.tags = ['internet']
 handler.command = /^(tetasgrandes)$/i
 export default handler
 
-global.pack = [
+global.tetasgrandes = [
   "https://www.copasmenstruales.com/blog/wp-content/uploads/Mal-olor-vaginal-768x512-1.jpg",
   "https://image.shutterstock.com/image-vector/daisy-flower-cute-bee-cartoon-260nw-1891391209.jpg",
   "https://i.pinimg.com/236x/2c/04/98/2c0498de02a52723c392ea08a149791d.jpg",
