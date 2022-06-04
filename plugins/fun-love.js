089c2a5ffb8ef6f16af751507dff6a0ccdb6ddbf
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-04 18:36:24 -0500

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, command, text }) => {
 conn.reply(m.chat, `
-*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
+*❤️❤️ 𝘮𝘦𝘥𝘪𝘥𝘰𝘳 𝘥𝘦 𝘢𝘮𝘰𝘳 ❤️❤️*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
-*Deberias pedirle que sea tu  novia/o ?*
+*Deberias pedirle que sea tu  novio/a ¡para que sean felices! uwu *
 `.trim(), m, m.mentionedJid ? {
 contextInfo: {
 mentionedJid: m.mentionedJid
