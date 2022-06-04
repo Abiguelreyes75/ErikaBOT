33a864c30441fad95c62973452d1c3ecc0a9359e
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-03 22:55:10 -0500

    Update nv-puerco.mp3.js

--- plugins/nv-puerco.mp3.js
+++ plugins/nv-puerco.mp3.js
@@ -4,11 +4,12 @@ import path from 'path'
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/puerco.mp3'
-conn.sendFile(m.chat, vn, 'baneado.mp3', null, m, true, {
+conn.sendFile(m.chat, vn, 'puerco.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /puerco|puerco/
+handler.customPrefix = /puerco|puerco/Puerco/
 handler.command = new RegExp
 export default handler
+
