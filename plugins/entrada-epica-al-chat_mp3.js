9e9eaeebbda4ab8f4a0085a87655c224f598dd61
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-05 23:22:01 -0500

    Update entrada-epica-al-chat_mp3.js

--- plugins/entrada-epica-al-chat_mp3.js
+++ plugins/entrada-epica-al-chat_mp3.js
@@ -1,11 +1,12 @@
-let util = require('util')
-let path = require('path')
+import util = from ('util'
+import path = from ('path')
 
 let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/entrada-epica-al-chat.mp3'
 conn.sendFile(m.chat, vn, 'entrada-epica-al-chat.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+type: 'audioMessage',
+ptt: true 
 })
 }
 handler.customPrefix = /ENTRADA|entrada|Entrada|Entra|ENTRA|Entra|Ingresa|ingresa|INGRESA|ingresar|INGRESAR|Ingresar/i 
