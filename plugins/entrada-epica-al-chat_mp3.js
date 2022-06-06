0b64150fe10d77339a94c5d4958b388f5a546583
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-05 23:25:09 -0500

    Update entrada-epica-al-chat_mp3.js

--- plugins/entrada-epica-al-chat_mp3.js
+++ plugins/entrada-epica-al-chat_mp3.js
@@ -1,14 +1,14 @@
-import util = from ('util'
-import path = from ('path')
+import util from 'util'
+import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/entrada-epica-al-chat.mp3'
 conn.sendFile(m.chat, vn, 'entrada-epica-al-chat.mp3', null, m, true, {
-type: 'audioMessage',
+type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /ENTRADA|entrada|Entrada|Entra|ENTRA|Entra|Ingresa|ingresa|INGRESA|ingresar|INGRESAR|Ingresar/i 
+handler.customPrefix = /ingresaloco|ingresaputa|ingreso/
 handler.command = new RegExp
-module.exports = handler
+export default handler
