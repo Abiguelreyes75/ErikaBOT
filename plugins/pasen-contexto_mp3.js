4991a7cb59093f673ec1e9dad540a26a15144bfb
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-05 23:40:03 -0500

    Update pasen-contexto_mp3.js

--- plugins/pasen-contexto_mp3.js
+++ plugins/pasen-contexto_mp3.js
@@ -11,4 +11,4 @@ ptt: true
 }
 handler.customPrefix = /Contexto|CONTEXTO|contexto|Pasen contexto|PASEN CONTEXTO|pasen contexto|Y el contexto|Y EL CONTEXTO|y el contexto/i
 handler.command = new RegExp
-module.exports = handler 
+export default handler 
