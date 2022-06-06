5f298ce98c9ef21b6d69eeef687c82e8daa222d2
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-05 23:38:00 -0500

    Update pasen-contexto_mp3.js

--- plugins/pasen-contexto_mp3.js
+++ plugins/pasen-contexto_mp3.js
@@ -1,7 +1,8 @@
-let util = require('util')
-let path = require('path')
+let util = from ('util')
+let path = from ('path')
 
 let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/contexto.mp3'
 conn.sendFile(m.chat, vn, 'contexto.mp3', null, m, true, {
 type: 'audioMessage', 
