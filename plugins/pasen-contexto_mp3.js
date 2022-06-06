7bf213ad6a64f48cbc5c347da46b0c9bf32afac0
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-05 23:49:02 -0500

    Update pasen-contexto_mp3.js

--- plugins/pasen-contexto_mp3.js
+++ plugins/pasen-contexto_mp3.js
@@ -1,14 +1,14 @@
-import util from ('util')
-import path from ('path')
+import util from 'util'
+import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/contexto.mp3'
 conn.sendFile(m.chat, vn, 'contexto.mp3', null, m, true, {
 type: 'audioMessage', 
-ptt: true 
+ptt: true
 })
 }
-handler.customPrefix = /Contexto|CONTEXTO|contexto|Pasen contexto|PASEN CONTEXTO|pasen contexto|Y el contexto|Y EL CONTEXTO|y el contexto/
+handler.customPrefix = /contexto|pasen contexto|Contexto porfa |contexti/
 handler.command = new RegExp
-export default handler 
+export default handle
