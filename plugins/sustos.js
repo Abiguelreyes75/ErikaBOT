5618ba1f7bd4a1e83b00e6d1eae4c17a408309b9
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-09 00:34:20 -0500

    Update sustos.js

--- plugins/sustos.js
+++ plugins/sustos.js
@@ -1,1 +1,15 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/sustos.mp3'
+conn.sendFile(m.chat, vn, 'sustos.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.command = /^(sustos|sustos que dan gusto|soliban|sutos gustos🤖)$/i
+handler.fail = null
+handler.exp = 100
+export default handler
