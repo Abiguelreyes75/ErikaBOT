76ed9b6fa487814da30a5899d43de863dbf13b72
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-10 23:18:51 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -330,6 +330,7 @@ _(𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)_
 ° ඬ⃟🔊 _algo anda mal_
 ° ඬ⃟🔊 _acercate viejo_
 ° ඬ⃟🔊 _donde esta tu honor basura_
+° ඬ⃟🔊 _Alan que pendejada hiciste_
 ° ඬ⃟🔊 _abduzcan_
 ° ඬ⃟🔊 _corre perra corre_
 ° ඬ⃟🔊 _si dicelo al juez rosadito_
