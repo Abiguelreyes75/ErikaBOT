724792447c2a2c3f3f49f18eb8a372cd98143b32
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-10 21:14:31 -0500

    Update estonoerapartedeltrato.js

--- plugins/estonoerapartedeltrato.js
+++ plugins/estonoerapartedeltrato.js
@@ -1,1 +1,14 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/estonoerapartedeltrato.mp3'
+    conn.sendFile(m.chat, vn, 'estonoerapartedeltrato.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /esto no era parte del trato|no era del trato|no fue parte del trato/
+handler.command = new RegExp
+export default handler
