8184e0e1a206f132de2e099a9f6e67561089f3e6
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-10 23:26:57 -0500

    Update alan-que-pendejada-hiciste.js

--- plugins/alan-que-pendejada-hiciste.js
+++ plugins/alan-que-pendejada-hiciste.js
@@ -2,13 +2,13 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
-    let vn = './media/alanquependejadahiciste.mp3'
-    conn.sendFile(m.chat, vn, 'alanquependejadahiciste.mp3', null, m, true, {
-        type: 'audioMessage',
-        ptt: true
-    })
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/alanquependejadahiciste.mp3'
+conn.sendFile(m.chat, vn, 'alanquependejadahiciste.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
 }
-handler.customPrefix = /alan que pendejada hiciste|Alan que pendejada hiciste|que pendejada hiciste/
+handler.customPrefix = /que pendejada hiciste|alan que pendejada hiciste/
 handler.command = new RegExp
-export default handle
+export default handler
