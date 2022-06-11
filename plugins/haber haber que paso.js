a6cf81b88f8b989563da4628263c162cffa3bafe
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-10 21:23:25 -0500

    Update haber haber que paso.js

--- plugins/haber haber que paso.js
+++ plugins/haber haber que paso.js
@@ -1,1 +1,15 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/quepaso.mp3'
+    conn.sendFile(m.chat, vn, 'quepaso.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /haber haber que paso|que paso|aver que paso/
+handler.command = new RegExp
+export default handler
 
