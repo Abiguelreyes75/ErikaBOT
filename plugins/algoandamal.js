09b599574be89b6b3b757bed60fde9ba356eeac2
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-10 22:10:04 -0500

    Update algoandamal.js

--- plugins/algoandamal.js
+++ plugins/algoandamal.js
@@ -1,1 +1,14 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/algoandamal.mp3'
+    conn.sendFile(m.chat, vn, 'algoandamal.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /algo anda mal|algoandamal|algo no anda bien/
+handler.command = new RegExp
+export default handler
