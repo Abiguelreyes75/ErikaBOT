26382edc71bf5253da3f8f7a29e35b8a5c81bea3
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-10 22:23:03 -0500

    Update casi-me-cago-del-susto.js

--- plugins/casi-me-cago-del-susto.js
+++ plugins/casi-me-cago-del-susto.js
@@ -1,1 +1,14 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/casimecagodelsusto.mp3'
+    conn.sendFile(m.chat, vn, 'casimecagodelsusto.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /casi me cago del susto|casimecagodelsusto|nomames casi me cago del susto/
+handler.command = new RegExp
+export default handler
