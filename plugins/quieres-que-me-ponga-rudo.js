fc5f606236059ebd58912365bae1df88cecdedc6
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-11 21:25:39 -0500

    Update quieres-que-me-ponga-rudo.js

--- plugins/quieres-que-me-ponga-rudo.js
+++ plugins/quieres-que-me-ponga-rudo.js
@@ -1,1 +1,14 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/quieresquemepongarudo.mp3'
+    conn.sendFile(m.chat, vn, 'quieresquemepongarudo.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /quieres que me ponga rudo|Quieres que me ponga rudo/
+handler.command = new RegExp
+export default handler 
