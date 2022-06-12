d5b8899d26ba20d6d1f94cc73947a98c7b9fc6c0
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-11 23:05:09 -0500

    Update adult-pack.js

--- plugins/adult-pack.js
+++ plugins/adult-pack.js
@@ -5,7 +5,7 @@ let url = pack[Math.floor(Math.random() * pack.length)]
 conn.sendButton(m.chat, `_🥵 Pack 🥵_`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 }
 handler.help = ['pack']
-handler.tags = ['internet']
+handler.tags = ['owner']
 handler.command = /^(pack)$/i
 export default handler
 
