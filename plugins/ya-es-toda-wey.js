29c16d36e46fc6f293ce3e645f4d95457b7e558e
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-11 20:56:01 -0500

    Update ya-es-toda-wey.js

--- plugins/ya-es-toda-wey.js
+++ plugins/ya-es-toda-wey.js
@@ -1,1 +1,14 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/yaestodawey.mp3'
+    conn.sendFile(m.chat, vn, 'yaestodawey.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /ya es toda wey|Ya es toda wey|ya es toda/
+handler.command = new RegExp
+export default handler 
