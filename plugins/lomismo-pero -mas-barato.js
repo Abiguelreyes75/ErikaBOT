78a5b6d56eea02fdecad6910c5c1c644ca6fcaf8
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-11 21:02:46 -0500

    Update lomismo-pero -mas-barato.js

--- plugins/lomismo-pero -mas-barato.js
+++ plugins/lomismo-pero -mas-barato.js
@@ -1,1 +1,14 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/lomismoperomasbarato.mp3'
+    conn.sendFile(m.chat, vn, 'lomismoperomasbarato.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /lo mismo pero mas barato|Lo mismo pero mas barato|lo mismo pero barato/
+handler.command = new RegExp
+export default handler
