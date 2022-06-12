e8041db05a8618f100c558b2e18f2d3433a7a727
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-11 20:47:18 -0500

    Update risadelarabe.js

--- plugins/risadelarabe.js
+++ plugins/risadelarabe.js
@@ -1,1 +1,14 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/risadelarabe.mp3'
+    conn.sendFile(m.chat, vn, 'risadelarabe.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /risa del arabe|Risa del arabe|risa/
+handler.command = new RegExp
+export default handler 
