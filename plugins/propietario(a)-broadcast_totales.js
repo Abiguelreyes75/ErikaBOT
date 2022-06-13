06abe7904f60babf81d14089dbbdaac220a2e8a4
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-12 23:26:19 -0500

    Update propietario(a)-broadcast_totales.js

--- plugins/propietario(a)-broadcast_totales.js
+++ plugins/propietario(a)-broadcast_totales.js
@@ -4,9 +4,9 @@ let chatsall = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).ma
 for (let id of chatsall) { 
 conn.sendButton(id, `*╭━━[ 𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 | 𝙉𝙊𝙏𝙄𝘾𝙀 ]━━━⬣*\n*┃*\n*┃💌* ${text}\n*┃*\n*╰━━━━━━━━━━━━━━━━━━⬣*`, '✅ *𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇*\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🎁 𝙄𝙣𝙛𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡', '.cuentasgb'],['🐈 𝙈𝙚𝙣𝙪', '.menu']], false, {
 contextInfo: { externalAdReply: {
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+title: 'Erika-BOT | Erika-BOT',
 body: 'Super Bot WhatsApp', 
-sourceUrl: `https://www.instagram.com/gata_dios`, 
+sourceUrl: `https://www.instagram.com/abiguelreyes75/`, 
 thumbnail: fs.readFileSync('./media/menus/Menu3.jpg') }}})}
 m.reply(`${iig} ✅ *El mensaje fue enviado a ${chatsall.length} Chats Totales*\n*Es posible que no se haya enviado a todos los Chats Totales. Disculpe.*\n\n✅ *The message was sent to ${chatsall.length} Totals Chats*\n*May not have been sent to all Totals Chats. Excuse me.*`)
 }
