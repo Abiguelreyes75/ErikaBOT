5dbb10229be2d0dac6b5cd701598f5b7eabf8015
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-12 23:25:17 -0500

    Update propietario(a)-broadcast-chats_privados.js

--- plugins/propietario(a)-broadcast-chats_privados.js
+++ plugins/propietario(a)-broadcast-chats_privados.js
@@ -4,9 +4,9 @@ let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g
 for (let id of chats) { 
 conn.sendButton(id, `*╭━━[ 𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 | 𝙉𝙊𝙏𝙄𝘾𝙀 ]━━━⬣*\n*┃*\n*┃💌* ${text}\n*┃*\n*╰━━━━━━━━━━━━━━━━━━⬣*`, '✅ *𝘾𝙊𝙈𝙐𝙉𝙄𝘾𝘼𝘿𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇*\n' + wm, fs.readFileSync('./src/avatar_contact.png'), [['🎁 𝙄𝙣𝙛𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡', '.cuentasgb'],['🐈 𝙈𝙚𝙣𝙪', '.menu']], false, {
 contextInfo: { externalAdReply: {
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+title: 'Erika-BOT | Erika-BOT',
 body: 'Super Bot WhatsApp', 
-sourceUrl: `https://www.instagram.com/gata_dios`, 
+sourceUrl: `https://www.instagram.com/abiguelreyes75/`, 
 thumbnail: fs.readFileSync('./media/menus/Menu3.jpg') }}})}
 m.reply(`${iig} ✅ *El mensaje fue enviado a ${chats.length} Chats Privados*\n*Es posible que no se haya enviado a todos los Chats Privados. Disculpe.*\n\n✅ *The message was sent to ${chats.length} Private Chats*\n*May not have been sent to all Private Chats. Excuse me.*`)
 }
