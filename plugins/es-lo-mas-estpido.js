df8a4a81fd959cdad0b3d298b91528f5ca5005e3
Abiguelreyes75 <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-15 01:41:13 -0500

    Update es-lo-mas-estpido.js

--- plugins/es-lo-mas-estpido.js
+++ plugins/es-lo-mas-estpido.js
@@ -1,1 +1,14 @@
+import util from 'util'
+import path from 'path'
 
+let handler = async (m, { conn }) => {
+    if(!db.data.chats[m.chat].audios && m.isGroup) throw 0
+    let vn = './media/eslomasestupidoqueehoido.mp3'
+    conn.sendFile(m.chat, vn, 'qeslomasestupidoqueehoido.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    })
+}
+handler.customPrefix = /es lo mas estupido que eh oido|Es lo mas estupido que eh oido|es lo mas estupido/
+handler.command = new RegExp
+export default handler 
