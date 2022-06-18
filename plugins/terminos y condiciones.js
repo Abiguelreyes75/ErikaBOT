bafe47922a5a899ce1406c01b458e4071092a0f8
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-18 01:35:47 -0500

    Update terminos y condiciones.js

--- plugins/terminos y condiciones.js
+++ plugins/terminos y condiciones.js
@@ -1,12 +1,18 @@
-let pp = './media/menus/Menu2.jpg'
-let str = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`
-
-conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/abiguelreyes75', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
-['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu']
-], m,)
-}
+import fs from 'fs'
+function handler(m, { conn }) {
+let text = `
+*𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤 | 𝘾𝙤𝙣𝙩𝙖𝙘𝙩* 
+*Wa.me/16479546631 (BOT)*
+*https://www.instagram.com/abiguelreyes75*
+`.trim()   
+conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: 'Erika-BOT | Erika-BOT',
+body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
+previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+sourceUrl: `https://wa.me/16479546631`}}})
+  
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-handler.command = /^(terminos|privacidad)$/i
+handler.command = /^(terminos)$/i
 export default handler
