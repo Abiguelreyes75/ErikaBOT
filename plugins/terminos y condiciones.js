1826249aab1a8aab6848570fbfe117c1b20510ef
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-18 01:36:46 -0500

    Update terminos y condiciones.js

--- plugins/terminos y condiciones.js
+++ plugins/terminos y condiciones.js
@@ -12,7 +12,18 @@ body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',
 previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
 sourceUrl: `https://wa.me/16479546631`}}})
   
+//const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+//this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+  
+let pp = './media/menus/Menu2.jpg'
+let str = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`
+
+conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/abiguelreyes75', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+['𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
+['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu']
+], m,)
+}
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
-handler.command = /^(terminos)$/i
+handler.command = /^(terminos|privacidad)$/i
 export default handler
