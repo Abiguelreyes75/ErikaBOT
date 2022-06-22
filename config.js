a52eb34ee6d8529b94f6e2cb77a08a83c39ff063
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-22 00:33:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
   ['16474952517','Erika-Bot', true],
-  ['16479546631'
+  ['16479546631']
 
 ]
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
