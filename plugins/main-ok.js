5e4545802769dddb9e0afe0db31717756087c171
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:25:10 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas"
-throw stiker.toString()
 }
 handler.tags = ['ok']
 handler.command = /^ok$/i
