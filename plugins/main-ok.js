60735661fd70c66581759c0500ab810adcdc387c
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:19:52 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -4,7 +4,6 @@ let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
 throw stiker.toString()
 }
-handler.help = ['ttp <teks>']
-handler.tags = ['sticker']
+handler.tags = ['ok']
 handler.command = /^ttp$/i
 export default handler
