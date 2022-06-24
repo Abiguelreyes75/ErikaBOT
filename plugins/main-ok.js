55b1d583a356378e22e17ec834829447dae1dc4f
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:56:51 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,5 +1,4 @@
 let handler = async (m, { conn, text }) => {
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
     
     conn.sendFile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png'})
     }
