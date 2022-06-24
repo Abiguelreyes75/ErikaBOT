7fa5a77304096487eb59b24240c75e938149f634
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:31:02 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,9 +1,18 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+import axios from "axios"
+let handler = async (m, {command, conn}) => {
+let res = await axios(pickRandom(meme))
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `_${command}_`.trim(), author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+}
+handler.help = ['ok']
+handler.tags = ['random']
+handler.command = /^(ok)$/i
+export default handler
 
-    conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
-    }
-    
-    handler.customPrefix = /^(ok)$/i
-    handler.command = new RegExp
-    
-    export default handler
+function pickRandom(list) {
+return list[Math.floor(list.length * Math.random())]}
+
+const meme = [
+"https://telegra.ph/file/f61a7c96f3f86b6c5214f.png"
+]
