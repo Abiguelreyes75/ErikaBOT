a4a07871c62e064d6368405e712b336323fb31eb
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:40:43 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn}) => {
 try {
 let { url } = json
 let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
-conn.sendFile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
 } catch (e) { }}
 handler.command = /^(ok)$/i
 export default handler
