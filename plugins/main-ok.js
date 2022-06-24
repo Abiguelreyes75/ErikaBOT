a10ad8dd176d9ec6f6520d801c09860c924a680a
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 22:31:31 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-    if (stiker) return conn.sendImageAsSticker(m.chat, stiker, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', '', m)
+    if conn.sendImageAsSticker(m.chat, stiker, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', '', m)
    throw stiker.toString()
     }
     
