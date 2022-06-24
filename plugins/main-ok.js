f9e6fe0d411db90c0bfbfa17ca33651afd2cb934
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:46:07 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,11 +1,10 @@
-import { sticker } from '../lib/sticker.js'
-import fetch from 'node-fetch'
-import MessageType from '@adiwajshing/baileys'
-let handler = async (m, { conn}) => {
-try {
-let { url } = json
-let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
-} catch (e) { }}
-handler.command = /^(ok)$/i
-export default handler
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  
+function
+    conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
+    }
+    
+    handler.customPrefix = /^(ok)$/i
+    handler.command = new RegExp
+    
+    module.exports = handler
