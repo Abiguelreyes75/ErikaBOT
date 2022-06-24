300d815e6c863b21da33ce97f4fd9e6c8c52feb1
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 22:09:54 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,5 +1,14 @@
 let 
     
+function conn.sendImageAsSticker(){
+
+
+    
+    document.getElementById("resultado").innerHTML='<img src="https://telegra.ph/file/f61a7c96f3f86b6c5214f.png" id="f61a7c96f3f86b6c5214f" style="width:30px; height:30px;
+   
+
+
+
     conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png'})
     }
     
