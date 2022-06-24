de2d972f982591c685594461ba7124ab27bcae3f
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 22:11:04 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -7,11 +7,6 @@ function conn.sendImageAsSticker(){
     document.getElementById("resultado").innerHTML='<img src="https://telegra.ph/file/f61a7c96f3f86b6c5214f.png" id="f61a7c96f3f86b6c5214f" style="width:30px; height:30px;
    
 
-
-
-    conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png'})
-    }
-    
     handler.customPrefix = /^(ok)$/i
     handler.command = new RegExp
     
