03b12ef9328094c7606b221bf68e3417d22760ef
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:20:20 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -5,5 +5,5 @@ conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.p
 throw stiker.toString()
 }
 handler.tags = ['ok']
-handler.command = /^ttp$/i
+handler.command = /^ok$/i
 export default handler
