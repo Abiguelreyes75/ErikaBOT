4c0820cd38238df9938a15e164fa323a99de9d52
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:54:30 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
     
-    conn.sendFile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
+    conn.sendFile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png'})
     }
     
     handler.customPrefix = /^(ok)$/i
