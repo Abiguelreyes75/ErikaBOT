4bcefde6930cb29721c143001e51deef3cc92082
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 22:40:21 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,8 +1,8 @@
 const dir = [
-  'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png',
-  'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png',
-  'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png',
-  'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png'
+  'https://img-03.stickers.cloud/packs/210a9e68-b249-405f-8ea1-9af015ef074a/webp/c5b7bded-e0f0-4f79-86aa-ffd825aba680.webp',
+  'https://img-03.stickers.cloud/packs/210a9e68-b249-405f-8ea1-9af015ef074a/webp/c5b7bded-e0f0-4f79-86aa-ffd825aba680.webp',
+  'https://img-03.stickers.cloud/packs/210a9e68-b249-405f-8ea1-9af015ef074a/webp/c5b7bded-e0f0-4f79-86aa-ffd825aba680.webp',
+  'https://img-03.stickers.cloud/packs/210a9e68-b249-405f-8ea1-9af015ef074a/webp/c5b7bded-e0f0-4f79-86aa-ffd825aba680.webp'
 ];
 let handler = async (m, { conn }) => {
 conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dadu.webp', '', m)
