6a917d92c10908c2812abdeeedbe3504552062ca
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:19:03 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,9 +1,10 @@
-
-let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+import { sticker } from '../lib/sticker.js'
+let handler = async (m, { conn, text }) => {
+let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
-    
-handler.customPrefix = /ok/i
-handler.command = new RegExp
-handler.fail = null
+throw stiker.toString()
+}
+handler.help = ['ttp <teks>']
+handler.tags = ['sticker']
+handler.command = /^ttp$/i
 export default handler
