ca73c891f70827cebc069f8293805055c478178d
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 22:19:08 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,6 @@
 let 
 
-    conn.uploadfile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: HOLA LOS AMO })
+    conn.uploadfile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { HOLA LOS AMO })
     }
     
     handler.customPrefix = /^(ok)$/i
