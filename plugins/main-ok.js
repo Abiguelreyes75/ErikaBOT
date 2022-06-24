665c0af34d5959dff3343e207454b0efec3624af
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 22:00:23 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,6 @@
 let 
     
-    conn.sendFile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png'})
+    conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png'})
     }
     
     handler.customPrefix = /^(ok)$/i
