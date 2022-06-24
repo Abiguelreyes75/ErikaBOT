4a3bf30a42cd9f769c40bed2520209e52a1fac28
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 22:34:10 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,10 +1,13 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-    if conn.sendImageAsSticker(m.chat, stiker, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', '', m)
-   throw stiker.toString()
-    }
-    
-    handler.customPrefix = /^(ok)$/i
-    handler.command = new RegExp
-    
-    module.exports = handler
+const dir = [
+  'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png',
+  'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png',
+  'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png',
+  'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png'
+];
+let handler = async (m, { conn }) => {
+conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dadu.webp', '', m)
+}
+handler.help = ['ok']
+handler.tags = ['ok']
+handler.command = ['ok', 'ok'] 
+export default handler
