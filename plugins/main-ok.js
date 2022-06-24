86846aa785d04118f471aa1f67e5e2f58936cd58
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 22:25:12 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     conn.uploadfile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
+   throw stiker.toString()
     }
     
     handler.customPrefix = /^(ok)$/i
