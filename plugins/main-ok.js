ba1e8d1de3a59de28bb138c2c5cfd93f43097861
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:24:37 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,7 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
-if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas"
 throw stiker.toString()
 }
 handler.tags = ['ok']
