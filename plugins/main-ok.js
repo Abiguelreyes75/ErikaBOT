e138bc3c393d4c2d83ea0f8b7a002e5afe1711be
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 21:39:50 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -3,10 +3,6 @@ import fetch from 'node-fetch'
 import MessageType from '@adiwajshing/baileys'
 let handler = async (m, { conn}) => {
 try {
-if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
-if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-let res = await fetch('https://telegra.ph/file/f61a7c96f3f86b6c5214f.png')
-let json = await res.json()
 let { url } = json
 let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
 conn.sendFile(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
