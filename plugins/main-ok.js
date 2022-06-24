33b8595f5d88323fef118a96a22f8e6482d94f96
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-23 20:55:01 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,5 +1,4 @@
-import fs from 'fs'
-function handler(m, { conn }) {
+let handler = async (m, { conn, usedPrefix, text }) => {
 
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
 }
