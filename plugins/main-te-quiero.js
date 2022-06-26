fa5614cc0cdd0f6c27488d4502f97189aafe674e
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-26 01:16:55 -0500

    `ERIKA-BOT`

--- plugins/main-te-quiero.js
+++ plugins/main-te-quiero.js
@@ -9,10 +9,10 @@ const dir = [
        //al poner te quiero  sin prefijo en el chat se envian los stickers 
 ];
 
-let hanler = async(m,{ conn }) => {
-    conn.sendfile(m.chat, dir[Math.floor(Math.random()*dir.length)],'sticker.webp', '',m)
+let hanler = async(m, { conn }) => {
+    conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'sticker.webp', '',m)
 
 }
-hanler.customprefix = /tequiero|Te quiero/
+handler.customprefix = /tequiero|Te quiero/
 handler.command = new RegExp
 export default handler
