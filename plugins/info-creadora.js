f323ee89f00113e5b20662964fda9e2c9b957c42
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-07-20 01:47:02 -0500

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -1,26 +1,43 @@
 import fs from 'fs'
-function handler(m, { conn }) {
-let text = `
-*𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤 | 𝘾𝙤𝙣𝙩𝙖𝙘𝙩* 
-*Wa.me/16479546631 (BOT)*
-*https://www.instagram.com/abiguelreyes75*
-`.trim()   
-conn.reply(m.chat, text, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: 'Erika-BOT | Erika-BOT',
-body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
-previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
-sourceUrl: `https://wa.me/16479546631`}}})
-  
-//const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-//this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+let handler = async (m, { conn, usedPrefix }) => {
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+let text = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`.trim()   
+let buttonMessage= {
+'document': { url: md },
+'mimetype': `application/${document}`,
+'fileName': `✦ 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 ༄`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': md,
+'mediaType': 2,
+'previewType': 'pdf',
+'title': 'Super Bot WhatsApp',
+'body': wm,
+'thumbnail': fs.readFileSync('./media/menus/Menu3.jpg'),
+'sourceUrl': yt }},
+'caption': text,
+'footer': wm,
+'buttons':[
+{buttonId: `${usedPrefix}donar`, buttonText: {displayText: '🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚'}, type: 1}, 
+{buttonId: `${usedPrefix}infobot`, buttonText: {displayText: '🐈 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞𝙤𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣'}, type: 1}, 
+{buttonId: `${usedPrefix}allmenu`, buttonText: {displayText: '💫 𝙈𝙚𝙣𝙪́ 𝙘𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪'}, type: 1}],
+'headerType': 6 }
+
+await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
   
 let pp = './media/menus/Menu2.jpg'
-let str = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`
+let str = `${wm}`
+let oficial = 'Comunícate con Mí Creadora por Instagram!!, Solo por ese medio puede ayudarte sobre Temas de GataBot\n\nCommunicate with My Creator on Instagram!!, Only by that means can help you on GataBot Themes'
 
-conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/abiguelreyes75', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
-['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu']
+await conn.sendHydrated2(m.chat, str, oficial, pp, 'LOS AMO', 'ERIKA-BOT', 'https://www.instagram.com/abiguelreyes75/', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', [
+['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu'],
 ], m,)
 }
 handler.help = ['owner', 'creator']
