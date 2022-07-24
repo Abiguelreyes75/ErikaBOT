ab13d9046e2eac398a4a8c62479620823330b67a
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-07-24 01:03:07 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,8 @@
 let handler = async(m, { conn }) => {
 if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${ag}𝙇𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝙀𝙎𝙏𝘼𝙉 𝘿𝙀𝙎𝘼𝘾𝙏𝙄𝙑𝘼𝘿𝙊𝙎 𝙐𝙎𝙀 *#on stickers* 𝙋𝘼𝙍𝘼 𝘼𝘾𝙏𝙄𝙑𝘼𝙍\n\n𝙏𝙃𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍𝙎 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 𝘼𝙍𝙀 𝘿𝙄𝙎𝘼𝘽𝙇𝙀𝘿 𝙐𝙎𝙀 *#on stickers* 𝙏𝙊 𝙀𝙉𝘼𝘽𝙇𝙀`
 
+let nombre = '🐈 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿'
+let nombre2 = '𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨'
 
 const dir = [
 
@@ -12,9 +14,11 @@ const dir = [
            //al poner ok  sin prefijo en el chat se envian los stickers PARAERIKABOT
     ];
     
-    let handler = async(m, { conn }) => {
-        conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'sticker.webp', '', m)
-        conn.sendFile(m.chat, stiker, null, { asSticker: true })
+    
+let stiker = await sticker(null, s[Math.floor(Math.random() * s.length)], nombre, nombre2)
+conn.sendFile(m.chat, stiker, null, { asSticker: true })
+        
+        
     
     }
     handler.customPrefix = /Ok|ok|okis|okys/
