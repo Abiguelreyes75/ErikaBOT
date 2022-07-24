b159a330245bfcd0209e1bfe9f05f088483d6218
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-07-24 02:05:39 -0500

    Update S-mariobros.js

--- plugins/S-mariobros.js
+++ plugins/S-mariobros.js
@@ -16,7 +16,7 @@ const s = [
 let stiker = await sticker(null, s[Math.floor(Math.random() * s.length)], nombre, nombre2)
 conn.sendFile(m.chat, stiker, null, { asSticker: true })
 }
-handler.customPrefix = /ja/i
+handler.customPrefix = /jaja/i
 handler.command = new RegExp
 handler.exp = 50
 export default handler
