b727374693f3cf7196cf4c4bd8cbf90fb076db79
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-07-24 00:51:58 -0500

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -4,7 +4,7 @@ if (!db.data.chats[m.chat].stickers && m.isGroup) throw `${ag}𝙇𝙊𝙎 𝘾�
 
 
 
-const s = [
+const dir = [
   
   'https://img-03.stickers.cloud/packs/210a9e68-b249-405f-8ea1-9af015ef074a/webp/c5b7bded-e0f0-4f79-86aa-ffd825aba680.webp',
   'https://img-14.stickers.cloud/packs/dd1af312-1193-4190-bd9f-0f718a673c7f/webp/25afa7d4-d836-4bb8-8038-ab122a9d0ef8.webp',
@@ -12,7 +12,7 @@ const s = [
   'https://img-15.stickers.cloud/packs/ebee02f5-9df4-4e75-8bd2-11a1aa9cc65b/webp/17ea85f7-05ca-4adc-96bd-21d47ad1a7fe.webp'
   ];
                                                   let handler = async (m, { conn }) => {
-                                                  conn.sendFile(m.chat, s[Math.floor(Math.random() * s.length)], 'sticker.webp', '', m)
+                                                  conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'sticker.webp', '', m)
   }
                                                   handler.customPrefix = /Ok|ok|okis|okys/
                                                   handler.command = new RegExp
