88f0bc858e86e5d5f20bb602f687f458948e6d3e
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-07-25 01:08:06 -0500

    Update menu-apostar.js

--- plugins/menu-apostar.js
+++ plugins/menu-apostar.js
@@ -98,7 +98,7 @@ conn.sendHydrated(m.chat, menu, wm, pp, 'MUY PRONTO', '𝐄𝐑𝐈𝐊𝐀-𝐁
 
 handler.help = ['infomenu'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
-handler.command = /^(apostar)$/i
+handler.command = /^(hacerapuesta)$/i
 //handler.register = true
 handler.exp = 70
 export default handler
