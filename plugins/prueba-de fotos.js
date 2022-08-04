eb922cb9a1b7396bf974801d35a01380f1edb07d
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-04 02:33:08 -0500

    Update prueba-de fotos.js

--- plugins/prueba-de fotos.js
+++ plugins/prueba-de fotos.js
@@ -3,7 +3,11 @@ let handler = async (m, { conn, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw'
 let url = prueba[Math.floor(Math.random() * prueba.length)]
 conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['🔄 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 🔄', `/${command}`]], m)
-  
+
+  let handler = async (m, { conn, command }) => {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw'
+let url = prueba[Math.floor(Math.random() * prueba.length)]
+conn.sendButton(m.chat, `_${command}_`.trim(), author, haha.data, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
 
   
   
