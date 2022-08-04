e6c68ac03d93f655373c26e23e2ebf3e1eca3a08
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-04 02:27:48 -0500

    Update prueba-de fotos.js

--- plugins/prueba-de fotos.js
+++ plugins/prueba-de fotos.js
@@ -17,7 +17,7 @@ handler.level = 5
 export default handler
 
 
-global.pies = [
+global.prueba = [
 "https://i.pinimg.com/originals/d9/e0/d4/d9e0d435743e9db03cf6b1b01627830f.jpg",
 "https://i.pinimg.com/474x/ef/75/1e/ef751e617f97136f3da8d841e967449d.jpg",
 "https://i.pinimg.com/564x/5d/b0/74/5db074236b157c385a0b8511218ed3e2.jpg",
