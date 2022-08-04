405cf0dff4e9d65ee89fef24cca43bdeeeaf6674
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-04 02:57:28 -0500

    Update prueba-de fotos.js

--- plugins/prueba-de fotos.js
+++ plugins/prueba-de fotos.js
@@ -2,15 +2,15 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw'
 
-  if (command == 'tiempo') {
+
   let url = prueba[Math.floor(Math.random() * prueba.length)]
 await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['🔄 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 🔄', `/${command}`]], m)
-await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, null, m)
-await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, null, m)
-await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, null, m)
-await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, null, m)
-await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, null, m)
-await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, null, m)
+await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['🔄 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 🔄', `/${command}`]], m)
+await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['🔄 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 🔄', `/${command}`]], m)
+await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['🔄 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 🔄', `/${command}`]], m)
+await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['🔄 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 🔄', `/${command}`]], m)
+await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['🔄 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 🔄', `/${command}`]], m)
+await conn.sendButton(m.chat, `🥵 ♥ PIES ♥  🥵`, author, url, [['🔄 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 🔄', `/${command}`]], m)
   
 }
 
