bc259a1461a0320a22c601c11985c75560ec741b
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-12 01:09:41 -0500

    Update marker-ephoto360.js

--- plugins/marker-ephoto360.js
+++ plugins/marker-ephoto360.js
@@ -9,7 +9,6 @@ if (!args[0]) throw '*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇�
 try {    
 if (command == '1917') {
 conn.reply(m.chat, '*[❗] 𝙴𝙻𝙰𝙱𝙾𝚁𝙰𝙽𝙳𝙾 𝚂𝚄 𝙳𝙸𝚂𝙴𝙽̃𝙾, 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾...*', m)
- conn.reply(m.chat, '*[❗] SI SU LOGO NO ES ENVIADO CORRECTAMENTE UTILICE MENOS PALABRAS.*', m)
 let res = `https://violetics.pw/api/ephoto360/1917?apikey=${Key360}&text=${response[0]}`     //ya esta 
 conn.sendFile(m.chat, res, 'error.jpg', null, m)}
 if (command == 'cute') {
@@ -21,6 +20,7 @@ conn.reply(m.chat, '*[❗] 𝙴𝙻𝙰𝙱𝙾𝚁𝙰𝙽𝙳𝙾 𝚂𝚄 �
 let res = `https://violetics.pw/api/ephoto360/cute-girl-gamer?apikey=${Key360}&text=${response[0]}`
 conn.sendFile(m.chat, res, 'error.jpg', null, m)}
 } catch {
+conn.reply(m.chat, '*[❗] SI SU LOGO NO ES ENVIADO CORRECTAMENTE UTILICE MENOS PALABRAS.*', m)
 conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*', m)    
 }}
 handler.command = /^1917|girlgamer|cute/i
