f66c3711eca91a123153dfb07c4f65bc43cbdd94
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-12 01:29:28 -0500

    Update marker-ephoto360.js

--- plugins/marker-ephoto360.js
+++ plugins/marker-ephoto360.js
@@ -22,7 +22,7 @@ conn.sendFile(m.chat, res, 'error.jpg', null, m)}
   if (command == 'monkey') {
 conn.reply(m.chat, '*[❗] 𝙴𝙻𝙰𝙱𝙾𝚁𝙰𝙽𝙳𝙾 𝚂𝚄 𝙳𝙸𝚂𝙴𝙽̃𝙾, 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾...*', m)
   conn.reply(m.chat, '*[❗] SI SU LOGO NO ES ENVIADO CORRECTAMENTE UTILICE MENOS PALABRAS.*', m)
-let res = `https://violetics.pw/api/ephoto360/monkey?apikey=${Key360}&text=${response[0]}&text2=$(response[0]}`           // solo para probar si no funciona quitar 
+let res = `https://violetics.pw/api/ephoto360/monkey?apikey=${Key360}&text=${response[0]}&text2=${response[1]}`           // solo para probar si no funciona quitar 
 conn.sendFile(m.chat, res, 'error.jpg', null, m)}
   
   
