69a53a25cf215e216b47369eab3862a6a3e47be4
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-08-13 18:59:02 -0500

    Update marker-ephoto360.js

--- plugins/marker-ephoto360.js
+++ plugins/marker-ephoto360.js
@@ -10,13 +10,19 @@ try {
 if (command == '1917') {
 conn.reply(m.chat, '*[❗] 𝙴𝙻𝙰𝙱𝙾𝚁𝙰𝙽𝙳𝙾 𝚂𝚄 𝙳𝙸𝚂𝙴𝙽̃𝙾, 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾...*', m)
   conn.reply(m.chat, '*[❗] SI SU LOGO NO ES ENVIADO CORRECTAMENTE UTILICE MENOS PALABRAS.*', m)
-let res = `https://violetics.pw/api/ephoto360/1917?apikey=${Key360}&text=${response[0]}`     //ya esta 
+let res = `https://violetics.pw/api/ephoto360/1917?apikey=${Key360}&text=${response[0]}`      
 conn.sendFile(m.chat, res, 'error.jpg', null, m)}
   
 if (command == 'cute') {
 conn.reply(m.chat, '*[❗] 𝙴𝙻𝙰𝙱𝙾𝚁𝙰𝙽𝙳𝙾 𝚂𝚄 𝙳𝙸𝚂𝙴𝙽̃𝙾, 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾...*', m)
   conn.reply(m.chat, '*[❗] SI SU LOGO NO ES ENVIADO CORRECTAMENTE UTILICE MENOS PALABRAS.*', m)
 let res = `https://violetics.pw/api/ephoto360/cute-typography?apikey=${Key360}&text=${response[0]}`
+conn.sendFile(m.chat, res, 'error.jpg', null, m)}
+  
+  if (command == 'clouds') {
+conn.reply(m.chat, '*[❗] 𝙴𝙻𝙰𝙱𝙾𝚁𝙰𝙽𝙳𝙾 𝚂𝚄 𝙳𝙸𝚂𝙴𝙽̃𝙾, 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾...*', m)
+  conn.reply(m.chat, '*[❗] SI SU LOGO NO ES ENVIADO CORRECTAMENTE UTILICE MENOS PALABRAS.*', m)
+let res = `https://violetics.pw/api/ephoto360/clouds-sky?apikey=${Key360}&text=${response[0]}`
 conn.sendFile(m.chat, res, 'error.jpg', null, m)}
   
   if (command == 'mylove') {
@@ -51,5 +57,5 @@ conn.sendFile(m.chat, res, 'error.jpg', null, m)}
 } catch {
 conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*', m)    
 }}
-handler.command = /^1917|girlgamer|cute|monkey|graffiti|galaxy1|mylove/i
+handler.command = /^1917|girlgamer|cute|monkey|graffiti|galaxy1|mylove|clouds/i
 export default handler
