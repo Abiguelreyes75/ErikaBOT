e49a0b65df0d480a68f2c73967f859348a4acf35
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-09-08 18:17:52 -0500

    Update info-rapidez.js

--- plugins/info-rapidez.js
+++ plugins/info-rapidez.js
@@ -3,8 +3,7 @@ let handler = async m => {
  let neww = performance.now()
  let speed = neww - old
  let txt = `${eg}*_COMENZANDO PRUEBA DE VELOCIDAD..._*\n*_STARTING SPEED TEST..._*`.trim()
-  m.reply(txt)
-let handler = async (m, { conn, usedPrefix, command, args, usedPrefix: _p, __dirname, isOwner, text, isAdmin, isROwner }) => 
+  m.reply(txt) 
 
 await m.reply('🚀')
 await m.reply('🚀🚀')
@@ -13,28 +12,12 @@ await m.reply(`╰⊱✅⊱ *𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊 | 𝙍𝙀𝙎
  
 let veloz = 
 `🚄 *VELOCIDAD:*\n *${speed}* *Milisegundos*\n\n🚄 *SPEED:*\n *${speed}* *Milliseconds*`
-
-
 let tm = {
 text: veloz,
 footer: global.wm,
 }
 conn.sendMessage(m.chat, tm, m)
 }
-const sections = [
-{
-title: `𝙇𝙄𝙎𝙏𝘼 𝘿𝙀𝙎𝙋𝙇𝙀𝙂𝘼𝘽𝙇𝙀 | 𝘿𝙍𝙊𝙋-𝘿𝙊𝙒𝙉 𝙇𝙄𝙎𝙏`,
-rows: [
-{title: "❇️ 𝙈𝙚𝙣𝙪 𝙋𝙧𝙞𝙣𝙘𝙞𝙥𝙖𝙡 |  𝘿𝙖𝙨𝙝𝙗𝙤𝙖𝙧𝙙 ❇️", description: null, rowId: `${usedPrefix}menu`},
- ]}, ]
-
-
-const listMessage = {
- 
- hello
- 
-buttonText: "𝙋𝙍𝙀𝙎𝙄𝙊𝙉𝙀 𝘼𝙌𝙐Í | 𝙋𝙍𝙀𝙎𝙎 𝙃𝙀𝙍𝙀", 
-sections }
 
 handler.help = ['ping']
 handler.tags = ['info']
