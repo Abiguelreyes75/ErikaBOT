7eb340a3f37f313f6335aa17dc9cca7b8f5992b6
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-09-20 01:49:42 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -55,7 +55,8 @@ let buttonMessage= {
 'caption': estado,
 'footer': wm,
 'buttons':[
-{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1}, 
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1},
+{buttonId: `${usedPrefix}hornymenu`, buttonText: {displayText: 'MENUXXX'}, type: 1},
 {buttonId: `${usedPrefix}donar`, buttonText: {displayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
 'headerType': 6 }
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
