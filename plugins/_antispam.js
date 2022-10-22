d31e580f6a2ff9ba050cb0e6c44f57da1501e236
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-10-21 20:09:27 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,7 +1,4 @@
 let handler = m => m
-
-if (!db.data.chats[m.chat].antispam && m.isGroup) throw 0
-
 handler.all = async function (m) {
 this.spam = this.spam ? this.spam : {}
 if (!(m.sender in this.spam)) {
