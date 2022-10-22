77aed27432232ccd58d7dc5d1595f3fb46bef5f2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-22 02:02:48 -0500

    Update _antispam-desbloquear.js

--- plugins/_antispam-desbloquear.js
+++ plugins/_antispam-desbloquear.js
@@ -6,10 +6,10 @@ return
 if (!user.desbloquear)
 return !0
 if (+new Date() > user.desbloquear) {
-let tiempo = [15000, 30000, 60000] * 1
+let tiempo = 60000 * 1
 setTimeout(() => {
 user.banned = false
-this.sendHydrated(m.chat, `*Fue desbloqueado después de ${tiempo / 1000} Segundos, NO HAGA SPAM*`, wm, null, null, null, null, null, [
+this.sendHydrated(m.chat, `*Fue desbloqueado después de ${tiempo / 1000 - 59} Minuto, NO HAGA SPAM*`, wm, null, null, null, null, null, [
 [null, null]], null)}, tiempo)
         
 user.desbloquear = null
