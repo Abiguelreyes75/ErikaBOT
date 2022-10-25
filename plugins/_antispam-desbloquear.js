a75d5e7aa0e66de936d34cb0b1c5ccefe1e27ff0
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-25 13:26:08 -0500

    Update _antispam-desbloquear.js

--- plugins/_antispam-desbloquear.js
+++ plugins/_antispam-desbloquear.js
@@ -1,5 +1,5 @@
 export async function all(m, conn) {
-//let username = conn.getName(m.sender)
+let texto
 let user = global.db.data.users[m.sender]  
 if (!m.message)
 return
@@ -9,8 +9,10 @@ if (+new Date() > user.desbloquear) {
 let tiempo = 60000 * 1
 setTimeout(() => {
 user.banned = false
-this.sendHydrated(m.chat, `*Fue desbloqueado después de ${tiempo / 1000 - 59} Minuto, NO HAGA SPAM*`, wm, null, null, null, null, null, [
-[null, null]], null)}, tiempo)
+texto = `*@${m.sender.split("@")[0]} FUE DESBLOQUEADO DESPUÉS DE 1 MINUTO, POR FAVOR NO HAGA SPAM!!*`
+this.sendButton(m.chat, texto, wm, null, [['☘️ 𝗠 𝗘 𝗡 𝗨', '/menu']], m, { mentions: this.parseMention(texto) })}, tiempo)
+//this.sendHydrated(m.chat, `*Fue desbloqueado después de ${tiempo / 1000 - 59} Minuto, NO HAGA SPAM*`, wm, null, null, null, null, null, [
+//[null, null]], null)}, tiempo)
         
 user.desbloquear = null
 }} 
