0bbf71c02fd26d913cd722dce929425522933599
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-10-25 15:48:12 -0500

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -37,6 +37,27 @@ await conn.reply(m.chat, texto,  m, { mentions: this.parseMention(texto) })
 user.banned = true
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 user.desbloquear = new Date * 1  
+  
+  
+//let texto
+//let user = global.db.data.users[m.sender]  
+if (!m.message)
+return
+if (!user.desbloquear)
+return !0
+if (+new Date() > user.desbloquear) {
+//let tiempo = 60000 * 1
+setTimeout(() => {
+user.banned = false
+texto = `*@${m.sender.split("@")[0]} FUE DESBLOQUEADO DESPUÉS DE 1 MINUTO, POR FAVOR NO HAGA SPAM!!*`
+//await this.reply(m.chat, texto,  m, { mentions: this.parseMention(texto) })}, tiempo)
+this.sendButton(m.chat, texto, wm, null, [['☘️ 𝗠 𝗘 𝗡 𝗨', '/menu']], m, { mentions: this.parseMention(texto) })}, tiempo)
+//this.sendHydrated(m.chat, `*Fue desbloqueado después de ${tiempo / 1000 - 59} Minuto, NO HAGA SPAM*`, wm, null, null, null, null, null, [
+//[null, null]], null)}, tiempo)
+        
+user.desbloquear = null
+}
+  
 } else {
 this.spam[m.sender].spam = 0
 this.spam[m.sender].lastspam = new Date * 1
