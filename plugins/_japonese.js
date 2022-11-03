cd53493f18c4a55cdd76fa9b216e5f5d5a03637a
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-11-02 19:36:36 -0600

    Update _japonese.js

--- plugins/_japonese.js
+++ plugins/_japonese.js
@@ -1,13 +1,50 @@
-let handler = async (m,{conn, arg, command, usedPrefix}) => {
-if(command == 'japonese'){
-try{
-await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*', m)
-let res = `https://violetics.pw/api/asupan/japan?apikey=${Key360}`
-await conn.sendFile(m.chat,res, 'error.jpg', null, m)}
+    let handler = async (m,{conn, arg, command, usedPrefix}) => { 
+    try{
+    if(command == 'japonese'){
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*', m)
+    let res = `https://violetics.pw/api/asupan/japan?apikey=${Key360}`
+    await conn.sendFile(m.chat, res, 'error.jpg', null, m)}
 
-catch {
-conn.reply(m.chat, 'error intete denuevo',m)
-}}
-}
-handler.command = /^japonese/i
-export default handler
+    if(command == 'vietnam'){
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*',m)
+    let res = `https://violetics.pw/api/asupan/vietnam?apikey=${Key360}`
+    await conn.sendFile(m.chat, res, 'error.jpg', null, m)}
+
+    if(command == 'thailand'){
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*',m)
+    let res = `https://violetics.pw/api/asupan/thailand?apikey=${Key360}`
+    await conn.sendFile(m.chat, res, 'error.jpg', null, m)
+
+    if(command == 'malaysia'){
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*',m)
+    let res = `https://violetics.pw/api/asupan/malaysia?apikey=${Key360}`
+    await conn.sendFile(m.chat, res, 'error.jpg', null , m)
+
+    if(command == 'korea'){
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*',m)
+    let res = `https://violetics.pw/api/asupan/korea?apikey=${Key360}`
+    await conn.sendFile(m.chat, res, 'error.jpg', null ,m)
+
+    if(command == 'indonesia'){
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*',m)
+    let res = `https://violetics.pw/api/asupan/indonesia?apikey=${Key360}`
+    await conn.sendFile(m.chat, res, 'error.jpg', null, m)
+
+    if(command = 'chinese'){
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*',m)
+    let res = `https://violetics.pw/api/asupan/chinese?apikey=${Key360}`
+    await conn.sendFile(m.chat, res, 'error.jpg', null, m)
+
+    if(command == 'cecan'){
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*',m)
+    let res = `https://violetics.pw/api/asupan/cecan?apikey=${Key360}`
+    await conn.sendFile(m.chat, res, 'error.jpg', null, m)
+
+    }}}}}}}
+    catch {
+    conn.reply(m.chat, 'error intente de nuevo',m)
+    }}
+    handler.command = /^japonese|vietnam|thailand|malaysia|korea|indonesia|chinese|cecan/i
+    handler.level = 2
+    handler.exp =50
+    export default handler
