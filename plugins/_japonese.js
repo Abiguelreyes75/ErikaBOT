d24ab5fb244e291512b2b98745fd19260e74e69e
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-11-02 18:18:40 -0600

    Update _japonese.js

--- plugins/_japonese.js
+++ plugins/_japonese.js
@@ -3,7 +3,7 @@ if (!arg[0])throw '*ingrese un texto*'
 if(command == 'japonese'){
 try{
 await conn.reply(m.chat, 'elaborando su foto espere...', m)
-let res = `https://violetics.pw/api/asupan/japan?apikey=${key360}&text=${response[0]}`
+let res = `https://violetics.pw/api/asupan/japan?apikey=964f-0c75-7afc`
 await conn.sendFile(m.chat,res, 'error.jpg', null, m)}
 
 catch {
