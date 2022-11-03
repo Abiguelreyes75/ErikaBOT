f2baec52da912b2cc3dec8e0a654f826f4034eb6
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-11-02 20:18:27 -0600

    Create poemaa.js

---
+++ plugins/poemaa.js
@@ -0,0 +1,12 @@
+let handler = async (m,{conn, command, usedPrefix}) =>{
+try{
+ if(command == 'poemaa'){
+ await conn.reply(m.chat, 'se esta enviando el poema',m)
+ let poemaa = `https://violetics.pw/api/random/pantun?apikey=964f-0c75-7afc`
+ await conn.senFile(m.chat, poemaa, 'error.jpg', null , m)}
+}
+  catch{
+  conn.reply(m.chat 'error intente de nuevo',m)
+  }}
+handler.command = /^poemma/i
+export default handler
