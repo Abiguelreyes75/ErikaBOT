ce3e893f19d8e4aefea83c33d752a660990b39b2
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-11-02 20:23:19 -0600

    Update poemaa.js

--- plugins/poemaa.js
+++ plugins/poemaa.js
@@ -1,9 +1,9 @@
 let handler = async (m,{conn, arg, command, usedPrefix}) => {
     if(command == 'poema'){
     try{
-    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 𝐟𝐨𝐭𝐨 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*', m)
+    await conn.reply(m.chat, '*[📷]𝐃𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐧𝐝𝐨 𝐬𝐮 POEMA 𝐞𝐬𝐩𝐞𝐫𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨...*', m)
     let res = `https://violetics.pw/api/random/pantun?apikey=${Key360}`
-    await conn.sendFile(m.chat,res, 'error.jpg', null, m)}
+    await conn.sendHidrated(m.chat,res, 'error.jpg', null, m)}
     
     catch {
     conn.reply(m.chat, 'error intete denuevo',m)
