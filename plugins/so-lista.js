e535485546ab87b9e7df26e60649204be12bc03f
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-11-12 12:09:21 -0600

    Update so-lista.js

--- plugins/so-lista.js
+++ plugins/so-lista.js
@@ -8,7 +8,7 @@ global.db.data.users[m.sender].exp += 50
     
 if (/^A Bueno master|Bueno master|Bueno Máster|🫂$/i.test(m.text) && chat.audios) {  
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
-let vn = './media/A bueno adios master.mp3'
+let vn = './media/elmamut.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
 
