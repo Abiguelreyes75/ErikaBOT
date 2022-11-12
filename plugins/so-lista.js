24216e57ea2a1c62db753058edfba1fab965b711
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-11-12 12:10:47 -0600

    Update so-lista.js

--- plugins/so-lista.js
+++ plugins/so-lista.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 global.db.data.users[m.sender].money += 50
 global.db.data.users[m.sender].exp += 50  
     
-if (/^A Bueno master|Bueno master|Bueno Máster|🫂$/i.test(m.text) && chat.audios) {  
+if (/^mamut|el mamut$/i.test(m.text) && chat.audios) {  
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
 let vn = './media/elmamut.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
