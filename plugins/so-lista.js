6f7274db4b0682dfffe2005d4be0125601f5120d
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-11-12 12:07:47 -0600

    Create so-lista.js

---
+++ plugins/so-lista.js
@@ -0,0 +1,16 @@
+//CREDITOS PARA >> https://github.com/BrunoSobrino
+
+let handler = m => m
+handler.all = async function (m) {
+let chat = global.db.data.chats[m.chat]
+global.db.data.users[m.sender].money += 50
+global.db.data.users[m.sender].exp += 50  
+    
+if (/^A Bueno master|Bueno master|Bueno Máster|🫂$/i.test(m.text) && chat.audios) {  
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
+let vn = './media/A bueno adios master.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
+
+return !0 }
+export default handler
